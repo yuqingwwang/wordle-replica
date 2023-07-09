253f86ed3db72cbb368b3c6eee61780b1798bdbb
Yuqing Wang <bibolikepeaches@gmail.com>
2023-07-09 13:17:19 +0100

    :bug: Row restriction added for mobile

--- scripts/handleEnter.js
+++ scripts/handleEnter.js
@@ -1,5 +1,6 @@
 const enterButton = document.querySelector('.enter-button');
-enterButton.addEventListener('click', () => {
+enterButton.addEventListener('click', (e) => {
+  const target = e.target;
   // Check if all the grid items are filled
   const gridItems = document.querySelectorAll('.grid-item');
   const isGridFilled = Array.from(gridItems).every(gridItem => gridItem.textContent.length > 0);
@@ -7,6 +8,14 @@ enterButton.addEventListener('click', () => {
     // Get the text from the grid items in the last 5 grid items
     const text = Array.from(gridItems).slice(-5).map(gridItem => gridItem.textContent);
     console.log(text);
+    // if there is less than 9 grid items, add a new row
+    if (gridItems.length > 9) {
+      if (!target.classList.contains('game-over')) {
+        target.classList.add('game-over');
+        alert('Game Over');
+      }
+      return;
+    }
     generateRow();
   }
 });

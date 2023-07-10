6ee54cc5b7b1021b13e476c318061f10d2830ee8
Yuqing Wang <bibolikepeaches@gmail.com>
2023-07-10 13:29:22 +0100

    :lipstick: Imporve keyboard layout

--- scripts/alphabets.js
+++ scripts/alphabets.js
@@ -6,26 +6,26 @@ function addAlphabets() {
   buttonsContainer.after(alphabetsContainer);
 
   const qwerty = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
-  const asdf = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', '👻'];
+  const asdf = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
   const zxcv = ['Z', 'X', 'C', 'V', 'B', 'N', 'M'];
 
+  const rows = [qwerty, asdf, zxcv];
+
   function populate(array) {
+    const rowContainer = document.createElement("div");
+    rowContainer.classList.add("row-container");
+    alphabetsContainer.appendChild(rowContainer);
+
     for (let i = 0; i < array.length; i++) {
       const alphabet = document.createElement("button");
       alphabet.classList.add("alphabet");
       alphabet.textContent = array[i];
-      alphabetsContainer.appendChild(alphabet);
-
+      rowContainer.appendChild(alphabet);
 
       alphabet.addEventListener('click', () => {
         // Target the next grid item that is empty
         const emptyGridItem = document.querySelector('.grid-item:empty');
         if (emptyGridItem) {
-          // if 👻 is pressed, do an alert
-        if (alphabet.textContent === '👻') {
-          alert('Boo!');
-          return;
-        }
           emptyGridItem.textContent = alphabet.textContent;
           emptyGridItem.focus();
         }
@@ -43,9 +43,9 @@ function addAlphabets() {
     }
   }
 
-  populate(qwerty);
-  populate(asdf);
-  populate(zxcv);
+  rows.forEach(row => {
+    populate(row);
+  });
 }
 
 addAlphabets();
